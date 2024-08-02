becd531011a7d0d8d29fb7ee882377e80a0bd9cd
Kimbeomsu3797 <qjatn3797@gmail.com>
2024-08-02 16:58:17 +0900

    ver 1.0.3

--- ProjectWareHouse/Assets/Scripts/PlayerMove.cs
+++ ProjectWareHouse/Assets/Scripts/PlayerMove.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     CharacterController cC;
+    float gravityForce = -20f;
+    bool isGround = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cC = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizon");
+        float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         Vector3 movePos = transform.forward * h + transform.right * v;
         cC.Move(movePos * Time.deltaTime);
-        
+        if (cC.collisionFlags==CollisionFlags.Below)
+        {
+            isGround = true;
+        }
+        else if (!isGround)
+        {
+
+        }
     }
 }
